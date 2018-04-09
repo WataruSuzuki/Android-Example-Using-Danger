0c040d149df52a583eb7528530ec4b05dfa97953
WataruSuzuki <wataru0406@gmail.com>
2018-04-09 22:10:00 +0900

    Inserted danger codes

--- library/src/main/java/com/example/library/HogeFuga.java
+++ library/src/main/java/com/example/library/HogeFuga.java
@@ -1,7 +1,26 @@
 package com.example.library;
 
 public class HogeFuga {
-    public static void hogefuga() {
-        System.out.println("Hoge Fuga!!");
+    private HogeFuga hogefuga;
+
+    public void hogefuga() {
+        assert false;
+        System.out.println(hogefuga != null ? hogefuga.toString() : null);
+    }
+
+    public enum Type {
+        HOGE, FUGA;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case HOGE:
+                    return "hogeeee!!";
+                case FUGA:
+                    return "fugaaaa!!";
+                default:
+                    return null;
+            }
+        }
     }
 }
