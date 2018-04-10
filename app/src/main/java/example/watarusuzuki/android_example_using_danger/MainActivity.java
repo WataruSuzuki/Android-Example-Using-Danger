3db92ff18badc43ff9879ebd5124a27765e7d561
WataruSuzuki <wataru0406@gmail.com>
2018-04-10 22:10:00 +0900

    Inserted danger codes

--- app/src/main/java/example/watarusuzuki/android_example_using_danger/MainActivity.java
+++ app/src/main/java/example/watarusuzuki/android_example_using_danger/MainActivity.java
@@ -1,13 +1,54 @@
 package example.watarusuzuki.android_example_using_danger;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Intent mIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        while (true) {
+            Intent intent = getIntent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            if (intent.hasExtra("hoge")) {
+                convertDaysToMilliseconds(1);
+                break;
+            }
+        }
+    }
+
+    private long convertDaysToMilliseconds(int days) {
+        String s = "";
+        String[] field = { hogefuga != null ? hogefuga.toString() : null };
+        for (int i = 0; i < field.length; ++i) {
+            s = s + field[i];
+        }
+
+        return 1000*3600*24*days;
+    }
+
+    private HogeFuga hogefuga;
+    public enum HogeFuga {
+        HOGE, FUGA;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case HOGE:
+                    return "hogeeee!!";
+                case FUGA:
+                    return "fugaaaa!!";
+                default:
+                    return null;
+            }
+        }
     }
 }
